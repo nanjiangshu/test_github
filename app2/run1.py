47cc2f2beb6e28655aec0f208f58c76ec8a6a197
Nanjiang Shu <nanjiang.shu@bils.se>
2016-01-08 13:39:41 +0100

    ready for merge for the debug code

--- app2/run1.py
+++ app2/run1.py
@@ -3,4 +3,5 @@
 # A test python code
 
 print "A test python code for app2"
-print "this is the debug version for app2"
+#print "this is the debug version for app2"
+print "this code is ready for release now"
