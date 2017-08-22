e02941a198a814376f08369910223b1d6244eb56
Nanjiang Shu <nanjiang.shu@gmail.com>
2017-08-22 14:23:29 +0200

    adding sleep 10s

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import random
+import time
 
 from app1.run import app1
 from app2.run import app2
@@ -18,7 +19,7 @@ if __name__ == '__main__':
         x3 = random.randint(a,b)
         ap1 = app1(x1,x2)
         ap2 = app2(x3)
-        print "loop = %d, x1 = %d, x2 = %d, x3 = %d"%(loop, x1,x2,x3)
+        print "\nloop = %d, x1 = %d, x2 = %d, x3 = %d"%(loop, x1,x2,x3)
         ap1.plus()
         ap1.multiplication()
         ap2.sin()
@@ -26,4 +27,5 @@ if __name__ == '__main__':
         ap2.pow10()
         ap2.pow16()
         ap2.tan()
+        time.sleep(10)
 
