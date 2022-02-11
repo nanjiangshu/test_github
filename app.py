516d27aff7dbbdf376f9b46cd1e8c89b66be578e
nanjiangshu <nanjiang.shu@gmail.com>
2022-02-11 22:35:05 +0100

    feat: trying to fix pylint error for app.py

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-# this script is for testing OpenShift continuous integration
-import os
-import sys
+"""this script is for testing OpenShift continuous integration"""
 import random
 import time
 
 from .app1.run import app1
 from .app2.run import app2
 
-if __name__ == '__main__':
-    # create a while loop for continuous running
+
+def main():
+    """main procedure"""
     loop = 0
-    while 1:
+    while loop < 10:
         loop += 1
-        a = 1
-        b = 100
-        x1 = random.randint(a,b)
-        x2 = random.randint(a,b)
-        x3 = random.randint(a,b)
-        ap1 = app1(x1,x2)
-        ap2 = app2(x3)
-        print("\nloop = %d, x1 = %d, x2 = %d, x3 = %d"%(loop, x1,x2,x3))
+        va = 1
+        vb = 100
+        vx1 = random.randint(va, vb)
+        vx2 = random.randint(va, vb)
+        vx3 = random.randint(va, vb)
+        ap1 = app1(vx1, vx2)
+        ap2 = app2(vx3)
+        print(f"loop = {loop}, vx1 = {vx1}, vx2 = {vx2}, vx3 = {vx3}")
         ap1.plus()
         ap1.multiplication()
         ap2.sin()
@@ -29,3 +28,7 @@ if __name__ == '__main__':
         ap2.tan()
         time.sleep(10)
 
+
+if __name__ == '__main__':
+    # create a while loop for continuous running
+    main()
