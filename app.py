59aa9cd7f9f40e98473a68bde6d2a2b1f28f1e92
nanjiangshu <nanjiang.shu@gmail.com>
2022-02-11 23:48:09 +0100

    feat: app does not need main

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from .app1.run import App1
 from .app2.run import App2
 
 
-def main():
-    """main procedure"""
+def run(max_loop=2):
+    """run procedure up to max_loop times"""
     loop = 0
-    while loop < 10:
+    while loop < max_loop:
         loop += 1
         a = 1
         b = 100
@@ -18,7 +18,7 @@ def main():
         x3 = random.randint(a, b)
         ap1 = App1(x1, x2)
         ap2 = App2(x3)
-        print(f"loop = {loop}, x1 = {x1}, x2 = {x2}, x3 = {x3}")
+        print(f"\nloop = {loop}, x1 = {x1}, x2 = {x2}, x3 = {x3}")
         ap1.plus()
         ap1.multiplication()
         ap2.sin()
@@ -27,8 +27,3 @@ def main():
         ap2.pow16()
         ap2.tan()
         time.sleep(10)
-
-
-if __name__ == '__main__':
-    # create a while loop for continuous running
-    main()
